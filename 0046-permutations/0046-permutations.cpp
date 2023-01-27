8a24960fe55eef8fd063079beecfb6884e1bfe46
roshanroxx <82316084+roshanroxx@users.noreply.github.com>
2023-01-27 19:19:39 +0530

    Time: 7 ms (34.72%), Space: 7.7 MB (79.82%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    // bool isSafe(unordered_set<int>& s,int n){
+    //     if(s.find(n)==s.end()){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    void pp(vector<vector<int>>& v,vector<int>& k,vector<int>& nums,int l,int n)
+    {
+        if(k.size()==n){
+            v.push_back(k);
+            return;
+        }
+        else{
+            for(int i=l;i<n;i++){
+                // if(isSafe(s,nums[i])){
+                // }
+                k.push_back(nums[i]);
+                swap(nums[i],nums[l]);
+                // s.insert(nums[i]);
+                pp(v,k,nums,l+1,n);
+                swap(nums[l],nums[i]);
+                k.pop_back();
+                // s.erase(nums[i]);
+            }
+        }  
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        unordered_set<int> s;
+        vector<vector<int>> v;
+        vector<int> k;
+        pp(v,k,nums,0,nums.size());
+        return v;
+    }
+};
