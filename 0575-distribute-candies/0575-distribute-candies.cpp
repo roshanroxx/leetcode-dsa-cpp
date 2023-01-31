0da9a7ce8c6c0c10698e43513985b9791efe56ea
roshanroxx <82316084+roshanroxx@users.noreply.github.com>
2023-01-31 21:03:41 +0530

    Time: 312 ms (65.25%), Space: 116.2 MB (48.92%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> s;
+        for(int x:candyType)
+        {
+            s.insert(x);
+        }
+        if(s.size()>candyType.size()/2)
+        {
+            return candyType.size()/2;
+        }
+        else{
+            return s.size();
+        }
+    }
+};
