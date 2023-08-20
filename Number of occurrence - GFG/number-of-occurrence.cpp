08116044a50595a7b4a231799d3cf4121ca0a151
roshanroxx <82316084+roshanroxx@users.noreply.github.com>
2023-08-20 10:56:47 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,53 +10,15 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-		int f(int arr[],int n,int x){
-            int l =0;
-            int r = n-1;
-            int ans = -1;
-            while(l<=r){
-                int mid = l +(r-l)/2;
-                if(arr[mid]==x){
-                   ans =mid;
-                   r=mid-1;
-                }
-                else if(arr[mid]>x){
-                    r = mid-1;
-                }
-                else{
-                    l = mid +1;
-                }
-            }
-            return ans;
-        }
-        
-        int l(int arr[],int n,int x){
-            int l =0;
-            int r = n-1;
-            int ans = -1;
-            while(l<=r){
-                int mid = l +(r-l)/2;
-                if(arr[mid]==x){
-                   ans =mid;
-                   l=mid+1;
-                }
-                else if(arr[mid]>x){
-                    r = mid-1;
-                }
-                else{
-                    l = mid +1;
-                }
-            }
-            return ans;
-        }
 	int count(int arr[], int n, int x) {
-	    int k = f(arr,n,x);
-        if(k==-1){
-            return 0;
-        }
-        int m = l(arr,n,x);
-        return m-k+1;
-    	}
+	    // code here
+	    
+	    
+	    int ll = lower_bound(arr,arr+n,x)  - arr;
+	    int up = upper_bound(arr,arr+n,x)  - arr;
+	    
+	    return up-ll;
+	}
 };
 
 //{ Driver Code Starts.
